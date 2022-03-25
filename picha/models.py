ed5f1d05797f69d2d66c556febe70ae004812db2
Mercy <mercycherotich757@gmail.com>
2022-03-25 19:54:24 +0300

    create an Image model

--- picha/models.py
+++ picha/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    image_link = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length =30)
+    description = models.TextField(max_length =30)
+    category = models.ForeignKey( 'Category', on_delete=models.CASCADE,)
+    location = models.ForeignKey( 'Location', on_delete=models.CASCADE,)
+
+
+
+
