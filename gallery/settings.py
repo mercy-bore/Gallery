49a6b8b1a9990edab423650d9c2c140ca0cbe4ce
Mercy <mercycherotich757@gmail.com>
2022-03-25 17:04:13 +0300

    list boostrap4 in my settings file as one of my installed apps

--- gallery/settings.py
+++ gallery/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'picha.apps.PichaConfig',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
