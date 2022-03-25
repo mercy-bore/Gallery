4974a0cb2e31c78cd30e535854973f796c357506
Mercy <mercycherotich757@gmail.com>
2022-03-25 16:03:03 +0300

    first view welcome

--- picha/urls.py
+++ picha/urls.py
@@ -2,3 +2,8 @@ from django.urls import re_path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+urlpatterns=[
+    re_path('^$',views.welcome,name = 'welcome'),
+
+]
--- picha/views.py
+++ picha/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
 import datetime as dt
 
+def welcome(request):
+    return HttpResponse('Welcome to My Gallery')
