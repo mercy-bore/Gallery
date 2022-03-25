5d37cb5fff196db08f7fbb9c7b7a07f481f78e86
Mercy <mercycherotich757@gmail.com>
2022-03-25 15:55:53 +0300

    create urls.py file in my picha app

---
+++ picha/urls.py
@@ -0,0 +1,4 @@
+from django.urls import re_path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
--- picha/views.py
+++ picha/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
+import datetime as dt
 
-# Create your views here.
