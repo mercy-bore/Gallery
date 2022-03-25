66a1eb9b556e5660e727a33ce7a3d6a55b1d6599
Mercy <mercycherotich757@gmail.com>
2022-03-25 16:27:05 +0300

    past_days_images view

--- picha/urls.py
+++ picha/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 urlpatterns=[
     re_path('^$',views.welcome,name = 'welcome'),
     re_path(r'^image/$',views.display_image,name='display_image'),
+    re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_images,name = 'pastImages'),
+    ] 
 
 
-]
+
--- picha/views.py
+++ picha/views.py
@@ -6,10 +6,26 @@ def welcome(request):
     return HttpResponse('Welcome to My Gallery')
 
 def display_image(request):
+    date = dt.date.today()  
     html = f'''
         <html>
             <body>
+                 <h1> Picture posted on  {date.day}-{date.month}-{date.year}</h1>
                 <h1><img src="https://www.freecodecamp.org/news/content/images/size/w2000/2021/08/chris-ried-ieic5Tq8YMk-unsplash.jpg"</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
+def past_days_images(request,past_date):
+    # Converts data from the string Url
+    date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+    day = (date)
+    html = f'''
+        <html>
+            <body>
+                <h1>Pictures for {day} {date.day}-{date.month}-{date.year}</h1>
             </body>
         </html>
             '''
